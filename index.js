2f7fa692ccd3da44f3304cc9e602a757491f27b0
tezalve <tezalve@gmail.com>
2023-05-02 00:31:39 +0600

    :tada: started & created 2 json file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 5000
+
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
+
+app.get('/', (req, res) => {
+    res.send('delish is running');
+})
+
+app.get('/chefs', (req, res) =>{
+    res.send(chefs);
+})
+app.get('/recipes', (req, res) =>{
+    res.send(recipes);
+})
+
+app.listen(port, () => {
+    console.log(`delish listening on port ${port}`)
+})
